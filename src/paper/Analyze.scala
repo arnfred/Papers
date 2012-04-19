95b2997db65887cc51b7e74728123aac3cb566c0
Jonas Arnfred <jonas@ifany.org>
2012-04-19 14:56:03 +0000

    After much general neglect, the files are finally updated on gitHub

---
+++ src/paper/Analyze.scala
@@ -0,0 +1,51 @@
+package paper
+
+object Analyze {
+  def main(args : Array[String]): Unit = {
+
+    // create analyse
+    val A : Analyzer = new Analyzer()
+
+    // Check that a directory is supplied
+    if (args.length == 0) println("You really need to supply a directory as argument");
+
+    // Then go ahead
+    else A.analyze(args(0))
+  }
+}
+
+class Analyzer extends Object with LoadPaper
+                              with ParsePaper 
+                              with ExtendPaper
+                              with ComparePaper
+                              with Graphs {
+
+  // Set a limit in percent for when papers get an edge between them
+  val limit : Int = 1
+
+  // Get cached papers in this order
+  val cache : List[String] = List(Cache.extended, Cache.linked, Cache.parsed)
+
+  // Set sources we want to extend with
+  val sources : List[PaperSource] = List(TalkDates, TalkRooms, PdfLink)
+
+  // Analyze a paper
+  def analyze(paperPos: String): Unit = {
+
+    // Get a list of parsed papers
+    val papers : List[Paper] = load(paperPos, cache, Isit)
+
+    // Compare the papers individually
+    val comparedPapers : List[Paper] = compare(papers, limit)
+
+    // Extend papers with tertiary data
+    val extendedPapers : List[Paper] = extend(comparedPapers, sources) 
+    
+    // Create graph
+    val graph : Graph = getGraph(extendedPapers)
+
+    // Print graph to file 'data.json'
+    graph.save
+
+  }
+}
