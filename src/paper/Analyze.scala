6109a228e76c3a38a4cd8b2bc051f3e079b87893
QJonny <jonny.quarta@epfl.ch>
2012-07-17 19:11:58 +0200

    Automatic commit

--- src/paper/Analyze.scala
+++ src/paper/Analyze.scala
@@ -6,7 +6,7 @@ object Analyze {
     // create analyse
     val A : Analyzer = new Analyzer()
 
-    // Check that a directory is supplied
+    // Check that a directory is supplied (the is an argument)
     if (args.length == 0) println("You really need to supply a directory as argument");
 
     // Then go ahead
